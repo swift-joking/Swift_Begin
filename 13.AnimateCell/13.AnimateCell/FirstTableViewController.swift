a192dc2005650b467410cc5981c604512e33928d
Roger_Chen_max5211314 <rong123020990@126.com>
2018-10-10 11:26:15 +0800

    AnimateCell

---
+++ 13.AnimateCell/13.AnimateCell/FirstTableViewController.swift
@@ -0,0 +1,105 @@
+//
+//  FirstTableViewController.swift
+//  13.AnimateCell
+//
+//  Created by 倩倩 on 2018/10/10.
+//  Copyright © 2018年 Swifter. All rights reserved.
+//
+
+import UIKit
+
+class FirstTableViewController: UITableViewController {
+
+    var tableData = ["Personal Life", "Buddy Company", "#30 days Swift Project", "Body movement training", "AppKitchen Studio", "Project Read", "Others" ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UIApplication.shared.isStatusBarHidden = true
+        
+        self.view.backgroundColor = UIColor.black
+        self.tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+        self.tableView.tableFooterView = UIView(frame: CGRect.zero)
+        self.tableView.register(FirstTableViewCell.self, forCellReuseIdentifier: "tableCell")
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        animateTable()
+        
+    }
+    
+    func animateTable() {
+        
+        self.tableView.reloadData()
+        
+        let cells = tableView.visibleCells
+        let tableHeight: CGFloat = tableView.bounds.size.height
+        
+        for i in cells {
+            let cell: UITableViewCell = i as UITableViewCell
+            cell.transform = CGAffineTransform(translationX: 0, y: tableHeight)
+        }
+        
+        var index = 0
+        
+        for a in cells {
+            
+            let cell: UITableViewCell = a as UITableViewCell
+            
+            UIView.animate(withDuration: 1.5, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: [], animations: {
+                
+                cell.transform = CGAffineTransform(translationX: 0, y: 0);
+                
+            }, completion: nil)
+            
+            index += 1
+        }
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell", for: indexPath)
+        
+        cell.textLabel?.text = tableData[indexPath.row]
+        cell.textLabel?.textColor = UIColor.white
+        cell.textLabel?.backgroundColor = UIColor.clear
+        cell.textLabel?.font = UIFont(name: "Avenir Next", size: 18)
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        return cell
+        
+    }
+    
+    func colorforIndex(_ index: Int) -> UIColor {
+        
+        let itemCount = tableData.count - 1
+        let color = (CGFloat(index) / CGFloat(itemCount)) * 0.6
+        return UIColor(red: color, green: 0.0, blue: 1.0, alpha: 1.0)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        cell.backgroundColor =  colorforIndex(indexPath.row)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        performSegue(withIdentifier: "secondVC", sender: nil)
+    }
+
+}
