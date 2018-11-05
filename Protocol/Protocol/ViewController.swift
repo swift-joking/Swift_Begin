18e720991f6bfc6696a281911abd626fb11c26c0
Roger_Chen_max5211314 <rong123020990@126.com>
2018-11-05 16:34:56 +0800

    protocol

---
+++ Protocol/Protocol/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Protocol
+//
+//  Created by 倩倩 on 2018/11/5.
+//  Copyright © 2018 Swifter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
---
+++ Protocol_Playground.playground/Contents.swift
@@ -0,0 +1,52 @@
+import UIKit
+
+// 协议
+protocol FullyNamed {
+    var fullName: String { get }
+}
+
+struct Person: FullyNamed {
+    var fullName: String
+}
+let john = Person(fullName: "John Appleseed")
+
+class Starship: FullyNamed {
+    var prefix: String?
+    var name: String
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    var fullName: String {
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+let ncc1790 = Starship(name: "Enterprise", prefix: "UUS")
+
+
+protocol Togglable {
+    mutating func toggle()
+}
+
+enum OnOffSwitch: Togglable {
+    case off, on
+    mutating func toggle() {
+        switch self {
+        case .off:
+            self = .on
+        case .on:
+            self = .off
+        }
+    }
+}
+
+// 泛型
+struct Stack<Element> {
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
